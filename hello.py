68a23778fad8838b1cec123a82b8454f82773497
Rashid Ali <72204718+rashidjatoi@users.noreply.github.com>
2022-11-12 09:39:06 +0500

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import tkinter
+
+# Create a root GUI Object
+root = tkinter
+
+# Crating a label Widget
+myLabel = Label(text="Hello World!")
+
+# Shoving it to the Screen
+myLabel.pack()
+
+# Creating loop for GUI
+root.mainloop()
